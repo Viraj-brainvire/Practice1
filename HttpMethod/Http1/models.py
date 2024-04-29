7ac9824aa1153b7f1cdd13acf77881d1545d3603
Viraj-Chauhan <viraj.chauhan@brainvire.com>
2024-04-29 18:52:00 +0530

    try to use reverse and update the error in codes

--- HttpMethod/Http1/models.py
+++ HttpMethod/Http1/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class data(models.Model):
-    personid = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=3)
+    personid = models.CharField(max_length=3)
     firstname = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=15)
     address = models.CharField(max_length=255,editable=True)
     city = models.CharField(max_length=255,editable=True)
+
+    def get_absolute_url(self):
+        return reverse("data_delete",kwargs={"id":self.id})
